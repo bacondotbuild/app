14a853209c99b266f9a1ed33ae9468357c395a05
bacon.build <bacondotbuild@gmail.com>
2023-02-27 21:29:16 -0800

    add app api route

---
+++ src/app/api/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  // const res = await fetch('https://data.mongodb-api.com/...', {
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'API-Key': process.env.DATA_API_KEY,
+  //   },
+  // })
+  // const data = await res.json()
+  const data = {
+    path: '/api',
+  }
+
+  return NextResponse.json(data)
+}
